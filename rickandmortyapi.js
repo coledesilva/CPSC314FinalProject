a6b807c049ba5c425ecc89fec7b97c16f18d709c
gagerg <gagerg@outlook.com>
2019-12-07 18:55:31 -0800

    randomizedCards functionality

--- rickandmortyapi.js
+++ rickandmortyapi.js
@@ -1,56 +1,68 @@
-function getState(state){
-    let endpoint = "https://api.openbrewerydb.org/breweries";
-    let queryString = "by_state=" + state;
-    let url = endpoint + "?" + queryString;
+function getCharacters(ids){
+    let endpoint = "https://rickandmortyapi.com/api/";
+    let characterString = "character/" + array;
+    let url = endpoint + characterString;
 
     let request = new XMLHttpRequest();
     request.open('GET', url,true);
 
     request.onload = function(){
-        let data = JSON.parse(this.response);
+    let data = JSON.parse(this.response);
 
-        for (let i = 0; i < data.length; i++){
-            console.log("Name: " + data[i].name + 
-            "\nBrewery Type: " + data[i].brewery_type +
-            "\nCity: " + data[i].city +
-            "\nState: " + data[i].state);
-        }
+    for(i = 0; i< data.length; i++){
+    console.log("ID: " + data[i].id +
+    "\nName: " + data[i].name +
+    "\nStatus: " + data[i].status +
+    "\nSpecies " + data[i].species);
     }
+}
 
     request.send();
 
-    //This fetch all of the data pertaining to the state
     fetch(url).then(function(result) {
-         return result.json();
+    return result.json();
     }).then(function(json) {
-        console.log(json);
+    console.log(json);
     });
-
-
-
 }
 
 
-function getCharacters(array){
-    let endpoint = "https://rickandmortyapi.com/api/";
-    let characterString = "character";
+function randomCards(){
+let endpoint = "https://rickandmortyapi.com/api/";
+    let characterString = "character/";
+
+    for(i = 0; i < 5; i++){
+        let x = Math.floor((Math.random() * 493)+1);
+        characterString = characterString + x +",";
+    }
+
+    let y = Math.floor((Math.random() * 493)+1);
+    characterString = characterString + y;
     let url = endpoint + characterString;
 
+    let cardImage = "homecardimg";
+    let cardTitle = "homecardtitle";
+    let cardText = "homecardtext";
+
     let request = new XMLHttpRequest();
+    request.open('GET', url,true);
+
     request.onload = function(){
-        let data = JSON.parse(this.response);
-        console.log("ID: " + data[i].id +
-        "\nName: " + data[i].name +
-        "\nStatus: " + data[i].status +
-        "\nSpecies " + data[i].species)
+    let data = JSON.parse(this.response);
+
+    for(i = 1; i<= data.length; i++){
+        let image = cardImage + i;
+        let title = cardTitle + i;
+        let text = cardText + i;
+
+        document.getElementById(image).src = data[i-1].image;
+        document.getElementById(title).innerHTML = data[i-1].name;
+        document.getElementById(text).innerHTML = data[i-1].species;
 
     }
+}
 
     request.send();
 
-    fetch(url).then(function(result) {
-         return result.json();
-    }).then(function(json) {
-        console.log(json);
-    });
+    console.log(url);
 }
